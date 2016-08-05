4e6d303243b439aae7aa52ece23c65d33a7d4db7
Allen <huxiaoming@live.com>
2016-08-05 09:56:07 +0800

    currently, we use branch 4.0 because main branch is not stable

--- debian/resources/fusionpbx.sh
+++ debian/resources/fusionpbx.sh
@@ -8,5 +8,6 @@ apt-get install -y --force-yes vim git dbus haveged ssl-cert
 apt-get install -y --force-yes ghostscript libtiff5-dev libtiff-tools
 
 #get the source code
-git clone https://github.com/fusionpbx/fusionpbx.git /var/www/fusionpbx
+#git clone https://github.com/fusionpbx/fusionpbx.git /var/www/fusionpbx
+git clone -b 4.0 https://github.com/hunering/fusionpbx.git /var/www/fusionpbx
 chown -R www-data:www-data /var/www/fusionpbx
